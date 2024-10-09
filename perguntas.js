b62bcbc4e03ee37d6857226fcea09974869d90d6
meas2512 <162702697+meas2512@users.noreply.github.com>
2024-10-09 09:19:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades',
+    'Quantas células o corpo humano possui?',
+    'Cerca de 37 trilhões.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual é a árvore mais alta do mundo?',
+    'A sequoia gigante.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual é o lugar mais seco do mundo?',
+    'O Deserto de Atacama, no Chile'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Quantas espécies de aves existem no mundo?',
+    'Aproximadamente 10.000 espécies.'
+)
